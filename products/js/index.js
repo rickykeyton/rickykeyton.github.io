cbfefd117f4ea26dfc8e20a4ff543f40bfc8546f
rickykeyton <rickykeyton3@gmail.com>
2017-05-16 23:06:41 +0800

    update

--- products/js/index.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "180px";
-    document.getElementById("main").style.marginLeft = "180px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}
